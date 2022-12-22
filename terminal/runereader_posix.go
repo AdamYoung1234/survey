cc708a353d26f8b1fa21188339974f17ae76321a
Chen Yang <adamyoung1234@outlook.com>
2022-12-22 17:49:57 -0600

    Convert to term package to avoid issues with Solaris

--- terminal/runereader_posix.go
+++ terminal/runereader_posix.go
@@ -13,8 +13,7 @@ import (
 	"bufio"
 	"bytes"
 	"fmt"
-	"syscall"
-	"unsafe"
+	"golang.org/x/term"
 )
 
 const (
@@ -23,7 +22,7 @@ const (
 )
 
 type runeReaderState struct {
-	term   syscall.Termios
+	term   *term.state
 	reader *bufio.Reader
 	buf    *bytes.Buffer
 }
@@ -45,30 +44,13 @@ func (rr *RuneReader) Buffer() *bytes.Buffer {
 
 // For reading runes we just want to disable echo.
 func (rr *RuneReader) SetTermMode() error {
-	if _, _, err := syscall.Syscall6(syscall.SYS_IOCTL, uintptr(rr.stdio.In.Fd()), ioctlReadTermios, uintptr(unsafe.Pointer(&rr.state.term)), 0, 0, 0); err != 0 {
-		return err
-	}
-
-	newState := rr.state.term
-	newState.Lflag &^= syscall.ECHO | syscall.ECHONL | syscall.ICANON | syscall.ISIG
-	// Because we are clearing canonical mode, we need to ensure VMIN & VTIME are
-	// set to the values we expect. This combination puts things in standard
-	// "blocking read" mode (see termios(3)).
-	newState.Cc[syscall.VMIN] = 1
-	newState.Cc[syscall.VTIME] = 0
-
-	if _, _, err := syscall.Syscall6(syscall.SYS_IOCTL, uintptr(rr.stdio.In.Fd()), ioctlWriteTermios, uintptr(unsafe.Pointer(&newState)), 0, 0, 0); err != 0 {
-		return err
-	}
-
-	return nil
+	var err error
+	rr.state.term, err = term.MakeRaw(int(rr.stdio.In.Fd()))
+	return err
 }
 
 func (rr *RuneReader) RestoreTermMode() error {
-	if _, _, err := syscall.Syscall6(syscall.SYS_IOCTL, uintptr(rr.stdio.In.Fd()), ioctlWriteTermios, uintptr(unsafe.Pointer(&rr.state.term)), 0, 0, 0); err != 0 {
-		return err
-	}
-	return nil
+	return term.Restore(int(rr.stdio.In.Fd()), rr.state.term)
 }
 
 // ReadRune Parse escape sequences such as ESC [ A for arrow keys.
